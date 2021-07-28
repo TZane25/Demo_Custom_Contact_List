7f90e3684b5b35728cee364a7fc9add1abe5cf78
TZane25 <20011280@myrp.edu.sg>
2021-07-28 10:01:44 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20011280/democustomcontactlist/MainActivity.java
@@ -0,0 +1,36 @@
+package sg.edu.rp.c346.id20011280.democustomcontactlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvContact;
+    ArrayList<Contact> alContactList;
+    CustomAdapter aaContact;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvContact = findViewById(R.id.listViewContacts);
+
+        alContactList = new ArrayList<>();
+        Contact item1 = new Contact("Mary",65,1234567,'F');
+        Contact item2 = new Contact("Ken",65,7654321,'M');
+        alContactList.add(item1);
+        alContactList.add(item2);
+
+        aaContact = new CustomAdapter(MainActivity.this,R.layout.row,alContactList);
+        lvContact.setAdapter(aaContact);
+
+    }
+}
